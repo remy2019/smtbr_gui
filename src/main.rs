2b2f77062958cd66ee50d46ac1b4cf76b0888e57
Seon Woong Yoon <remy2019@gmx.us>
2024-11-17 19:29:16 +0900

    impl exit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::process::exit;
+
 slint::include_modules!();
 
 fn main() {
     use slint::Model;
 
     let main_window = MainWindow::new().unwrap();
+    main_window.on_close(|| exit(0));
 
     main_window.run().unwrap();
 }
